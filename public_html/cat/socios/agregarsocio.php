92c034a37f1d5e50adf2c396551ca05150a12f31
xhenryx <hjhenriq@uc.cl>
2011-11-21 21:57:35 -0300

    cambios en addsocio y agregarsocio, para incluir pago

--- public_html/cat/socios/agregarsocio.php
+++ public_html/cat/socios/agregarsocio.php
@@ -22,21 +22,44 @@ $qrut="SELECT * from $tabla where rut_$tabla = $rut";
 $contador=$dbh->query($qrut);
 
 if($dbh->query($qrut))
-	$repetido=false;
-else
 	$repetido=true;
+else
+	$repetido=false;
 
 $sql=
 "INSERT INTO socio VALUES ('$rut', '$email', '$nombre', '$apellido_paterno', '$apellido_materno', '$sexo',
 '$comuna', '$direccion', localtimestamp(0), '$fechanacimiento', '$telefono1', '$telefono2')";
 Debugger::notice($sql);
 $dbh->exec($sql);
+
+//agregar el pago de matricula
+$tipomatricula=$_POST['tipomatricula'];
+$tipopago=$_POST['tipopago'];
+
+$monto=30000;
+$meses='3 months';
+
+if($tipomatricula == '12 meses')
+	{
+		$monto=2000;
+		$meses='12 months';
+	}
+if($tipomatricula == '6 meses')
+	{
+		$monto=2500;
+		$meses='6 months';
+	}
+
+$sql= "INSERT INTO matriculas VALUES ('$rut',localtimestamp(0),'',localtimestamp(0)+$meses,$tipopago,$monto)";	
+Debugger::notice($sql);
+$dbh->exec($sql);
+
 $dbh=null;
 
 if($repetido)
 echo "No se ha ingresado el nuevo socio, porque ese rut ya existe";
 else
-echo "Se ha ingresado un nuevo socio de nombre $nombre";
+echo "Se ha ingresado un nuevo socio de nombre $nombre su matricula dura por $meses";
 }
 catch(PDOException $e){
 	Debugger::notice($e->getMessage());
@@ -45,4 +68,4 @@ catch(PDOException $e){
 ?>	
 
 <p><a href="../../index.php?cat=socios&action=addsocio"> Volver a agregar otro socio</a></p>
-<p><a href="../../index.php"> Volver a la página principal</a></p>
+<p><a href="../../index.php"> Volver a la p&aacute;gina principal</a></p>
